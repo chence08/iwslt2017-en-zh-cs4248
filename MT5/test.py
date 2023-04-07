12298dc64a7336a394c4e0ef5bd2c7468ee324c0
Chen YiJia <65040076+chence08@users.noreply.github.com>
2023-04-07 16:29:45 +0800

    update test script

--- MT5/test.py
+++ MT5/test.py
@@ -1,8 +1,9 @@
 from datasets.load import load_dataset
+from evaluate import load
 import logging
-import sacrebleu
 import pandas as pd
 from simpletransformers.t5 import T5Model, T5Args
+from pprint import pprint
 
 raw_datasets = load_dataset('iwslt2017', 'iwslt2017-zh-en')
 
@@ -16,18 +17,19 @@ model_args.max_length = 512
 model_args.length_penalty = 1
 model_args.num_beams = 10
 
-model = T5Model("mt5", "outputs", args=model_args)
+model = T5Model("mt5", "mt5_more_epochs/checkpoint-115634-epoch-2", args=model_args)
+bleu = load('sacrebleu')
 
 en_zh_test = pd.DataFrame(raw_datasets['test']['translation'])
 zh_truth = en_zh_test['zh'].tolist()
 en_input = en_zh_test['en'].tolist()
 
-zh_preds = model.predict(en_input)
-en_zh_bleu = sacrebleu.corpus_bleu(zh_preds, zh_truth)
+zh_preds = model.predict(en_input) # time consuming
+en_zh_bleu = bleu.compute(predictions=zh_preds, references=zh_truth, tokenize='zh')
 print("----------------------------------------------")
-print("English to Chinese: ", en_zh_bleu.score)
+print("English to Chinese: ", en_zh_bleu)
 
 en_preds = model.predict(zh_truth)
-zh_en_bleu = sacrebleu.corpus_bleu(en_preds, en_input)
+zh_en_bleu = bleu.compute(predictions=en_preds, references=en_input)
 print("----------------------------------------------")
-print("Chinese to English: ", zh_en_bleu.score)
+print("Chinese to English: ", zh_en_bleu)
